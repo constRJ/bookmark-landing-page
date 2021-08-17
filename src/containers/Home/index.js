d360881fda582fa68109f09fdb4e7e1941698b41
captmjay <commander.mjay23@gmail.com>
2021-08-17 17:45:14 +0800

    project setup

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,10 +1,8 @@
+import TopNavbar from "@components/TopNavbar"
 const HomePage = () => {
   return (
     <>
-      <div className="container-xxl">
-        <h1>Hello bootstrap</h1>
-        <button className="btn btn-softRed">Custom</button>
-      </div>
+      <TopNavbar />
     </>
   )
 }
