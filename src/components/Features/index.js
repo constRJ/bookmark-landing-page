11ded33e53c889268acbcc0cfb5304dfdecc3ff6
captmjay <commander.mjay23@gmail.com>
2021-08-26 12:09:13 +0800

    added features section

---
+++ src/components/Features/index.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+const Features = () => {
+  return (
+    <React.Fragment>
+      <section className="section-features-container">
+        <div className="container-xxl">
+          <div className="col-9 mx-auto">
+            <h2 className="text-center">Features</h2>
+            <p className="text-center">Our aim is to make it quick and easy for you to access your favourite websites. Your bookmarks sync between your devices so you can access them on the go.</p>
+          </div>
+        </div>
+      </section>
+    </React.Fragment>
+  )
+}
+
+export default Features
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,12 +1,13 @@
 import Banner from "@components/Banner";
+import Features from "@components/Features";
 import TopNavbar from "@components/TopNavbar";
-
 const HomePage = () => {
   return (
     <>
       <TopNavbar />
       <main>
         <Banner />
+        <Features />
       </main>
     </>
   )
